68de11715cdffb3b84fa3872f3cb3b824c37e08e
hunte <besscomdark@gmail.com>
2023-02-18 10:37:07 +0800

    添加分數時間資料

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace monster
 {
@@ -8,6 +10,10 @@ namespace monster
     {
 
         public int m_ScoreValue = 0;
+        public float m_TimeValue = 0;
+
+        public TextMeshProUGUI m_Score = null;
+        public TextMeshProUGUI m_Time = null;
 
         void Start()
         {
